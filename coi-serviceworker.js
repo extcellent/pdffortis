2c3cffa9feb9c39cd079d7667e4e03642119e467
extcellent <tkjucker@gmail.com>
2026-06-19 11:25:39 +0200

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", function(event) {
-  if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
   event.respondWith(
     fetch(event.request).then(function(response) {
+      if (!response || response.status === 0 || !response.body) return response;
       const newHeaders = new Headers(response.headers);
       newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
       newHeaders.set("Cross-Origin-Embedder-Policy", "credentialless");
@@ -12,6 +12,6 @@ self.addEventListener("fetch", function(event) {
         statusText: response.statusText,
         headers: newHeaders,
       });
-    })
+    }).catch(() => fetch(event.request))
   );
 });
